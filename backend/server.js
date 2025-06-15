32093c2cda95d6e357e562e3f7166b2d6b70d61d
Lam Huu Tho <thopk135@gmail.com>
2025-06-16 00:03:28 +0700

    done

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,13 @@ import dotenv from 'dotenv'
 import { protectRoute } from './middleware/protectRoute.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
 
 dotenv.config();
 const app = express();
-const __dirname = path.resolve();
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
